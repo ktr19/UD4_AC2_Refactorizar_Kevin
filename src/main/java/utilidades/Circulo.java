22769bdd10a4a6bfa42ef36b9b7420d75e02b209
kevin <>
2024-03-03 21:25:13 +0100

    0.9.2.Correccion de la tabulacion del codigo.

--- src/main/java/utilidades/Circulo.java
+++ src/main/java/utilidades/Circulo.java
@@ -1,7 +1,8 @@
 package utilidades;
 
 public class Circulo {
-	private double rad;
+
+    private double rad;
 
     /**
      * @return the rad
@@ -17,38 +18,33 @@ public class Circulo {
         this.rad = rad;
     }
 
-	public Circulo(double radio)
-	    	{
-	        this.rad = radio;
-	}
+    public Circulo(double radio) {
+        this.rad = radio;
+    }
 
-	    	public void imprimir()
-	    	{
-	        String color = "rojo";
-	        System.out.println("Di�metro: " + 2*getRad());
-	        System.out.println("Color: " + color);
-	        double area = 2 * 3.1416 * getRad() * getRad();
-	        System.out.println(area);
-	}
+    public void imprimir() {
+        String color = "rojo";
+        System.out.println("Di�metro: " + 2 * getRad());
+        System.out.println("Color: " + color);
+        double area = 2 * 3.1416 * getRad() * getRad();
+        System.out.println(area);
+    }
 
-	public boolean esIgual (Circulo otro, boolean conDecimales)
-	{
-	        double radio1 = this.getRad();
-	        double radio2 = otro.getRad();
-	        if (conDecimales)
-	        {
-	            if (radio1 == radio2) 
-	              return true;
-	            else
-	              return false;
-	        }
-	        else
-	        {
-	            if (Math.abs(radio1-radio2)<1)
-	                return true;
-	            else
-	                return false;
-	        }
-	    }
-	}
-
+    public boolean esIgual(Circulo otro, boolean conDecimales) {
+        double radio1 = this.getRad();
+        double radio2 = otro.getRad();
+        if (conDecimales) {
+            if (radio1 == radio2) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (Math.abs(radio1 - radio2) < 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
