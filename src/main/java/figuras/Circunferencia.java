46f16b64d18b736d63da63dfefffcc130be094b1
kevin <>
2024-03-03 21:33:39 +0100

    0.9.6 .Se ha renombrado el atributo "rad" por "radio".

--- src/main/java/figuras/Circunferencia.java
+++ src/main/java/figuras/Circunferencia.java
@@ -2,37 +2,37 @@ package figuras;
 
 public class Circunferencia {
 
-    private double rad;
+    private double radio;
 
     /**
-     * @return the rad
+     * @return the radio
      */
-    public double getRad() {
-        return rad;
+    public double getRadio() {
+        return radio;
     }
 
     /**
-     * @param rad the rad to set
+     * @param radio the radio to set
      */
-    public void setRad(double rad) {
-        this.rad = rad;
+    public void setRadio(double radio) {
+        this.radio = radio;
     }
 
     public Circunferencia(double radio) {
-        this.rad = radio;
+        this.radio = radio;
     }
 
     public void imprimir() {
         String color = "rojo";
-        System.out.println("Di�metro: " + 2 * getRad());
+        System.out.println("Di�metro: " + 2 * getRadio());
         System.out.println("Color: " + color);
-        double area = 2 * 3.1416 * getRad() * getRad();
+        double area = 2 * 3.1416 * getRadio() * getRadio();
         System.out.println(area);
     }
     
     public boolean esIgual(Circunferencia otro, boolean conDecimales) {
-        double radio1 = this.getRad();
-        double radio2 = otro.getRad();
+        double radio1 = this.getRadio();
+        double radio2 = otro.getRadio();
         if (conDecimales) {
             if (radio1 == radio2) {
                 return true;
